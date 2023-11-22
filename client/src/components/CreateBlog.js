98713408439232a76e2c2ee4f6baf0311f59170d
Mheyrie <omota4fidelity@gmail.com>
2023-11-22 20:19:01 +0100

    updated

--- client/src/components/CreateBlog.js
+++ client/src/components/CreateBlog.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {Form, Button} from 'react-bootstrap'
+import{useForm} from 'react-hook-form'
 
 function CreateBlog() {
+  
+  const{register, handleSubmit, reset, formState:{errors}} = useForm()
+
+
   return (
-    <div>CreateBlog</div>
+    <div className="container_blog">
+      <h1>Create New Blog</h1>
+      <form>
+        <Form.Group>
+          <Form.Label>Title: <br/></Form.Label>
+          <Form.Control className="title" type="text"/>
+        </Form.Group>
+        <br/>
+        <Form.Group>
+          <Form.Label>
+            Body: <br/>
+            </Form.Label>
+          <Form.Control className="body" as="textarea" rows={5} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Min to read: </Form.Label>
+          <Form.Control/>
+        </Form.Group>
+        <Form.Group>
+          <br/>
+          <Button variant="primary">
+            Save
+          </Button>
+        </Form.Group>
+      </form>
+    </div>
   )
 }
 
